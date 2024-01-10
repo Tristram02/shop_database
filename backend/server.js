9e11a816973758070a527a95dba2280e51e78f8a
Tristram02 <wldoekpl@gmail.com>
2024-01-10 21:39:24 +0100

    Products and categories

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const sql = require('mysql');
 const cors = require('cors');
 const { check, validationResult } = require("express-validator");
-const { ReasonPhrases, StatusCodes } = require("http-status-code");
+const {
+	ReasonPhrases,
+	StatusCodes,
+} = require('http-status-codes');
 
 const app = express();
 app.use(cors());
+app.use(express.json({limit:'1mb'}))
 
 const db = sql.createConnection({
     host: "localhost",
@@ -15,12 +19,12 @@ const db = sql.createConnection({
 });
 
 const validateProduct = [
-    check("nazwa").notEmpty().withMessage("Nazwa produktu nie może być pusta"),
-    check("opis").notEmpty().withMessage("Opis produktu nie może być pusty"),
-    check("cena")
+    check('Nazwa').notEmpty().withMessage("Nazwa produktu nie może być pusta"),
+    check("Opis").notEmpty().withMessage("Opis produktu nie może być pusty"),
+    check("Cena jednostkowa")
       .isFloat({ min: 0 })
       .withMessage("Cena produktu musi być liczbą większą lub równą 0"),
-    check("waga")
+    check('Waga jednostkowa')
       .isFloat({ min: 0 })
       .withMessage("Waga produktu musi być liczbą większą lub równą 0"),
 ];
@@ -40,12 +44,87 @@ app.get('/products', (req, res) => {
         .status(StatusCodes.INTERNAL_SERVER_ERROR)
         .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
     }
+});
+
+app.get('/products/:id', (req, res) => {
+    const productID = req.params.id;
+
+    try {
+        db.query('select * from produkt where ID_produktu = ?', [productID], (err, data) => {
+            if (data.length > 0) {
+                return res.json(data);
+            } else {
+                res.status(404).json({error: "Nie znaleziono produktu"});
+            }
+        });
+    } catch (err) {
+        console.log("Error: ", err);
+        res.
+        status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+    }
 })
 
 app.post('/products', async (req, res) => {
     const newProduct = req.body;
-    console.log(newProduct);
-})
+    
+    try {
+        const result = db.query("insert into produkt set ?", [newProduct]);
+        res.json({productID: result.insertId})
+    } catch (err) {
+        console.log("Error: ", err);
+        res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+    }
+});
+
+app.put('/products/:id', validateProduct, (req, res) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        console.log(error);
+        return res.status(StatusCodes.BAD_REQUEST).json({"error": error.array()});
+    }
+
+    const productID = req.params.id;
+    const newProduct = req.body;
+
+    try {
+        db.query('select * from produkt where ID_produktu = ?', [productID], (err, data) => {
+            if (data.length === 0) {
+                return res.status(404).json({error: "Nie znaleziono produktu"});
+            } 
+
+            db.query('update produkt set ? where ID_produktu = ?', [
+                newProduct,
+                productID
+            ]);
+            res.json({ success: true });
+        });
+    } catch (err) {
+        console.log("Error: ", err);
+        res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+    }
+});
+
+app.get('/categories', (req, res) => {
+    const query = "select * from kategoria";
+    try {
+        db.query(query, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            return res.json(data);
+        })
+    } catch (err) {
+        console.log("Error: ", err);
+        return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send(ReasonPhrases.INTERNAL_SERVER_ERROR);
+    }
+});
 
 
 app.listen(5000, (err) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,23 @@ import React, {useEffect, useState} from 'react'
 
 function App() {
   const [data, setData] = useState([]);
+  const [kategorie, setKategorie] = useState([]);
+
+  const newProduct = {
+    "Cena jednostkowa": 70,
+    "Kategoria towaru": "Odzież",
+    "Nazwa": "Bluza",
+    "Opis": "Bluza męska rozmiar L",
+    "Waga jednostkowa": 20
+  };
+
+  const updatedProduct = {
+    "Cena jednostkowa": 70,
+    "Kategoria towaru": "Odzież",
+    "Nazwa": "Bluza",
+    "Opis": "Bluza męska rozmiar L",
+    "Waga jednostkowa": 40
+  }
 
   useEffect(() => {
     fetch('http://localhost:5000/products')
@@ -11,11 +28,38 @@ function App() {
     .then(data => setData(data))
     .catch(err => console.log(err));
   }, [])
-  data.map(item => {
-    console.log(item);
-  })
+  
+  useEffect(() => {
+    fetch('http://localhost:5000/categories')
+    .then(res => res.json())
+    .then(data => setKategorie(data))
+    .catch(err => console.log(err));
+  }, [])
+
+  async function handleAddProduct() {
+    await fetch('http://localhost:5000/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newProduct)
+    })
+  }
+
+  async function handleUpdateProduct() {
+    await fetch('http://localhost:5000/products/6', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedProduct)
+    })
+  }
+
+
   return (
     <div className="App">
+      <h2>Produkty</h2>
       <table>
         <thead>
           <th>ID</th>
@@ -34,6 +78,22 @@ function App() {
               <td>{item["Cena jednostkowa"]}</td>
               <td>{item["Waga jednostkowa"]}</td>
               <td>{item["Kategoria towaru"]}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <button onClick={handleAddProduct}>Add product</button>
+      <button onClick={handleUpdateProduct}>Update product</button>
+      <h2>Kategorie</h2>
+      <table>
+        <thead>
+          <th>Nazwa</th>
+        </thead>
+        <tbody>
+          {kategorie.map((item, index) => (
+            <tr key={index}>
+              <td>{item["Nazwa"]}</td>
             </tr>
           ))}
         </tbody>
